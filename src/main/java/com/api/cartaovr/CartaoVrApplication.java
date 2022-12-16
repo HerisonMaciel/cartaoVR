6c595f4abda6c9f880f8bafdd5e69233fe8d7884
HerisonMaciel <herisson2maciel@hotmail.com>
2022-12-16 17:13:15 -0300

    inicio

---
+++ src/main/java/com/api/cartaovr/CartaoVrApplication.java
@@ -0,0 +1,13 @@
+package com.api.cartaovr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartaoVrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartaoVrApplication.class, args);
+	}
+
+}
